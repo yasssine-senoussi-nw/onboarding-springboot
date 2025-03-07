54b4bbd60ad3b8bbf49197049b33075ffa633071
Yassine Senoussi <yassine.senoussi@theodo.com>
2025-03-07 10:40:37 +0000

    test: add unit tests the purchase jpa repository

--- api/src/test/java/com/nimbleways/springboilerplate/features/purchases/domain/ports/PurchaseRepositoryPortContractTests.java
+++ api/src/test/java/com/nimbleways/springboilerplate/features/purchases/domain/ports/PurchaseRepositoryPortContractTests.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static com.nimbleways.springboilerplate.testhelpers.fixtures.NewPurchaseFixture.aNewPurchase;
+
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -65,6 +66,67 @@ public abstract class PurchaseRepositoryPortContractTests {
 
         // THEN
         assertEquals(List.of(entity), purchases);
+    }
+
+    @Test
+    @Transactional
+    void findCoworkersPurchases_inexistent_userid_yields_empty() {
+        // GIVEN
+        UUID userId = UUID.randomUUID();
+
+        // WHEN
+        List<Purchase> purchases = purchaseRepositoryPort.findCoworkersPurchases(userId).toList();
+
+        // THEN
+        assertEquals(0, purchases.size());
+    }
+
+    @Test
+    @Transactional
+    void findCoworkersPurchases_no_other_user_yields_empty() {
+        // GIVEN
+        User user = userRepositoryPort.create(aNewUser().build());
+
+        // WHEN
+        List<Purchase> purchases = purchaseRepositoryPort.findCoworkersPurchases(user.id()).toList();
+
+        // THEN
+        assertEquals(0, purchases.size());
+    }
+
+    @Test
+    @Transactional
+    void findCoworkersPurchases_no_purchase_yields_empty() {
+        // GIVEN
+        User user1 = userRepositoryPort.create(aNewUser().build());
+        User user2 = userRepositoryPort.create(aNewUser().build());
+
+        // WHEN
+        List<Purchase> purchases1 = purchaseRepositoryPort.findCoworkersPurchases(user1.id()).toList();
+        List<Purchase> purchases2 = purchaseRepositoryPort.findCoworkersPurchases(user2.id()).toList();
+
+        // THEN
+        assertEquals(0, purchases1.size());
+        assertEquals(0, purchases2.size());
+    }
+
+    @Test
+    @Transactional
+    void findCoworkersPurchases_with_purchases_yields_correct_result() {
+        // GIVEN
+        User user1 = userRepositoryPort.create(aNewUser().build());
+        User user2 = userRepositoryPort.create(aNewUser().build());
+
+        Purchase entity1 = purchaseRepositoryPort.create(aNewPurchase().build(user1.id()));
+        Purchase entity2 = purchaseRepositoryPort.create(aNewPurchase().build(user2.id()));
+
+        // WHEN
+        List<Purchase> purchases1 = purchaseRepositoryPort.findCoworkersPurchases(user1.id()).toList();
+        List<Purchase> purchases2 = purchaseRepositoryPort.findCoworkersPurchases(user2.id()).toList();
+
+        // THEN
+        assertEquals(List.of(entity2), purchases1);
+        assertEquals(List.of(entity1), purchases2);
     }
 
     @Test
